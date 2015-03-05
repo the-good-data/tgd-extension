692fd2573c88a25d6ae706a5280c65b041703ac6
bodiapz <bodiapz@gmail.com>
2015-03-05 12:10:07 +0200

    Manual input to user seniority levels solved #137

--- chrome/scripts/popup.js
+++ chrome/scripts/popup.js
@@ -369,10 +369,13 @@ function writeContributed(percentileData){
   }
   
   var text = percentileData.level,
-      img = text.toLowerCase() + '.png';
-  
+      img = percentileData.icon,
+      color = percentileData.color;
+
   $('#layer_usertype_title').html(text.toUpperCase());
-  $('#layer_usertype_image').attr("src","../images/"+img)
+  //$('#layer_usertype_title').css('color', color);
+  $('#layer_usertype_image').attr("src",TGD_API+"uploads/seniority/"+img)
+  //$('#layer_usertype_image').attr("src","../images/"+img)
     .addClass('icon ' + text.toLowerCase());// not every icon is te same so some css styling must be applied.
 }
 
